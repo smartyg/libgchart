14fe9a54d2cad1e6d07e870ea7ab4559611e5e61
Martijn Goedhart <goedhart.martijn@gmail.com>
2022-04-05 23:06:18 +0200

    include copyright header in source files

--- src/g_chart.c
+++ src/g_chart.c
@@ -1,4 +1,21 @@
 /* kate: indent-mode cstyle; tab-width 4; indent-width 4; */
+/*
+ * g_chart.c
+ * Copyright (C) Martijn Goedhart 2022 <goedhart.martijn@gmail.com>
+ *
+ * This is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "config.h"
 
 #include "g_chart.h"
