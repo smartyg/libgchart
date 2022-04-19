f391103c65e578d17c7d2962d72e51b604769f12
Martijn Goedhart <goedhart.martijn@gmail.com>
2022-04-19 20:56:52 +0200

    update example for gtk4

--- examples/chart-gtk4.cpp
+++ examples/chart-gtk4.cpp
@@ -16,11 +16,13 @@ MyWindow::MyWindow()
 {
 	set_title("Basic application");
 	set_default_size(700, 700);
-	std::map<const float, const float> map1 = {{0,0},{1,10},{2,10},{3,15},{4,5},{5,10},{5,11},{6,0}};
+	std::map<const float, const float> map1A = {{0,0},{1,10},{2,10},{3,15},{4,5},{5,10},{5,11},{6,0}};
+	std::map<const float, const float> map1B = {{0,0},{1,11},{2,11},{3,16},{4,6},{5,9},{5,10},{6,0}};
 	std::map<const float, const float> map2 = {{0,100},{1.1,120},{1.5,140},{2,150},{3.1,110},{3.8,80},{4.5,91},{5.1,95},{5.5,105},{6,100}};
 
 	this->chart.setLabels ("afstand", "km", nullptr, "snelheid", "km/u", nullptr, "hoogte", "m", nullptr);
-	this->chart.addY1Chart (GchartChart::Type::LINEAR, 1, {1, 0, 0} , map1, nullptr);
+	this->chart.addY1Chart (GchartChart::Type::LINEAR, 1, {1, 0, 0} , map1A, nullptr);
+	this->chart.addY1Chart (GchartChart::Type::LINEAR, 2, {0, 0, 1} , map1B, nullptr);
 	this->chart.addY2Chart (GchartChart::Type::LINEAR, 1, {0, 1, 0} , map2, nullptr);
 	set_child (chart);
 }
